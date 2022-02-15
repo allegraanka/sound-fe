1dd1e7ccdc856dda583d3f0281302df65a1efa57
Allegra Anka <allegraanka@Allegras-MacBook-Pro.local>
2022-02-14 22:40:22 -0500

    strapi updates

--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout/Layout';
-
-export default function AboutPage() {
-    return (
-        <Layout title='The Sound | About Us'>
-            <h1>About The Sound</h1>
-        </Layout>
-    );
-}
--- strapi/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'a1efb4277a3661151932076e34a7bf7e'),
-  },
-});
